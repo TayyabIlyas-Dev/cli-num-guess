2dc3b530bddf92374d03e1de27b3ea24aa4034ca
TAYYAB ILYAS <tht9935@gmail.com>
2024-03-23 00:52:46 +0500

    created num-geuss-game

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+//                    [project  CLI NUM GUESS]
+// my first target is
+// 1) make a code to genrate a random  number      -Done
+// 2) user input                                   -Done
+// 3) compare user input with random num which genrated
+// const randomNumber = 13;
+// const answer = await inquirer.prompt([
+//     {
+//         name:"userinput",
+//         message:"please guess a number",
+//         type:"number",
+//     }
+// ]);
+// if(randomNumber === answer.userinput){
+//     console.log("you Win --- U guess a Same NUmber");
+// }
+// else{
+//     console.log("you Lose --- U guess A Different Number");
+// }
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userinput",
+        message: "please guess a number Between 1-5",
+        type: "number",
+    }
+]);
+if (randomNumber === answer.userinput) {
+    console.log("you Win --- U guess a Same NUmber ");
+    console.log(`Answer is ${randomNumber}`);
+}
+else {
+    console.log("you Lose --- U guess A Different Number");
+    console.log(`Answer is ${randomNumber}`);
+}
